d091e78e568bc884853ceb3a2cc7c0ef602f64ca
ArturoBarrios <arturo.barrios@theplanworks.com>
2020-06-13 16:50:34 -0400

    created containers for header, body, and footer

--- pages/GlobalComponents/header.js
+++ pages/GlobalComponents/header.js
@@ -4,16 +4,16 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header(props){
     return(
-        <div style= {{width: "100%"}}> 
+        <div> 
        
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">
             <img
                 alt=""
                 src="/microphone.svg"
-                width="30%"
-                height="100%"
-                className="d-inline-block align-top"
+                
+                
+                // className="d-inline-block align-top"
             />{' '}
             
             Piano Song Grade Predictor
