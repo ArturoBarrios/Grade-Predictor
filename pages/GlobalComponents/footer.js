510216a17367765250de34ba52b1304ee1b27661
ArturoBarrios <arturo.barrios@theplanworks.com>
2020-06-13 17:36:08 -0400

    added template cbody component to index.js

--- pages/GlobalComponents/footer.js
+++ pages/GlobalComponents/footer.js
@@ -1,1 +1,30 @@
 
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
+function Footer(props){
+    return(
+        <div style={{width:"100%"}} > 
+       
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+            <img
+                alt=""
+                src="/microphone.svg"
+                style={{ height: 100, width: 236 }}
+
+                
+                
+                className="d-inline-block align-middle"
+            />{' '}
+            
+            Piano Song Grade Predictor
+            </Navbar.Brand>
+        </Navbar>
+    </div>
+    );
+
+}
+
+export default Footer;
--- pages/GlobalComponents/header.js
+++ pages/GlobalComponents/header.js
@@ -4,16 +4,18 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header(props){
     return(
-        <div> 
+        <div style={{width:"100%"}} > 
        
         <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">
             <img
                 alt=""
                 src="/microphone.svg"
+                style={{ height: 100, width: 236 }}
+
                 
                 
-                // className="d-inline-block align-top"
+                className="d-inline-block align-middle"
             />{' '}
             
             Piano Song Grade Predictor
