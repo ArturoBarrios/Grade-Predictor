dd282f718ac5e74d1b2c75a5c5cee043227f0077
ArturoBarrios <arturo.barrios@theplanworks.com>
2020-07-08 20:24:04 -0400

    fixed bug with feature values that were set to None

--- pages/GlobalComponents/cardsComponent.jsx
+++ pages/GlobalComponents/cardsComponent.jsx
@@ -14,25 +14,31 @@ import CardDeck from 'react-bootstrap/CardDeck'
                                 <Card.Body>
                                 <Card.Title>How to use</Card.Title>
                                 <Card.Text>
-                                    This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This content is a little bit longer.
+                                    First, choose a list of music files that you want to grade.<br></br><br></br>
+                                    Finally use any model of your choosing to grade your piece(s).<br></br><br></br>
+                                    Reference the Model Predictor to see outline of what each model does. <br></br><br></br>
+                                    Supported file formats include the following:<br></br> ABC, Capella,  
+                                    ClerqTemperley, Humdrum, MEI, MIDI, MuseData, MusicXML, Noteworthy, 
+                                    NoteworthyBinary, RomanText, Scala, TinyNotation, and Volpiano. This list 
+                                    might be updated. Check out <a href="https://web.mit.edu/music21/doc/usersGuide/usersGuide_08_installingMusicXML.html">this link </a>
+                                    to see all the available file formats. 
+
                                 </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
-                                <small className="text-muted">Last updated 3 mins ago</small>
+                               
                                 </Card.Footer>
                             </Card>
                             <Card>
                                 <Card.Img style={{"height": 200}} variant="top" src="model.svg" />
                                 <Card.Body>
-                                <Card.Title>Predictor</Card.Title>
+                                <Card.Title>Grade Predictors</Card.Title>
                                 <Card.Text>
-                                    This card has supporting text below as a natural lead-in to additional
-                                    content.{' '}
+                                   Display a list of available models with short descriptions here
                                 </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
-                                <small className="text-muted">Last updated 3 mins ago</small>
+                                <small className="text-muted"></small>
                                 </Card.Footer>
                             </Card>
                             <Card>
@@ -40,13 +46,11 @@ import CardDeck from 'react-bootstrap/CardDeck'
                                 <Card.Body>
                                 <Card.Title>Updates</Card.Title>
                                 <Card.Text>
-                                    This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This card has even longer content than the first to
-                                    show that equal height action.
+                                    Mention plans for future updates
                                 </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
-                                <small className="text-muted">Last updated 3 mins ago</small>
+                                <small className="text-muted"></small>
                                 </Card.Footer>
                             </Card>
                         </CardDeck>
