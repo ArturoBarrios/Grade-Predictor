00dc310961224258e83052f14558e1a637548fd9
ArturoBarrios <arturo.barrios@theplanworks.com>
2020-07-23 18:44:31 -0400

    fixed errors coming up after running brew install ffmpeg..

--- api/myproject/venv/bin/activate.fish
+++ api/myproject/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/arturobarrios/Documents/grade-predictor/api/myproject/venv"
+set -gx VIRTUAL_ENV "/Users/arturobarrios/Downloads/Grade-Predictor/api/myproject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
